c3c7db97a231711cf0299cc437d6cf4af85c7676
Ryan Johansen <ryankjohansen@gmail.com>
2017-02-18 10:57:44 +0200

    Added a UI Playgroud project and linked some functionality from a lib to the UI project

---
+++ src/InspectionsLib/InspectionsLib/InspectionsConstants.swift
@@ -0,0 +1,16 @@
+//
+//  InspectionsConstants.swift
+//  InspectionsLib
+//
+//  Created by Ryan Johansen on 2017/02/18.
+//  Copyright © 2017 Ryan Johansen. All rights reserved.
+//
+
+import Foundation
+
+public class InspectionsConstants {
+    
+    public init() {}
+    
+    public let ApplicationName : String = "Inspections"
+}
---
+++ src/InspectionsLib/InspectionsLib/InspectionsLib.h
@@ -0,0 +1,19 @@
+//
+//  InspectionsLib.h
+//  InspectionsLib
+//
+//  Created by Ryan Johansen on 2017/02/18.
+//  Copyright © 2017 Ryan Johansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InspectionsLib.
+FOUNDATION_EXPORT double InspectionsLibVersionNumber;
+
+//! Project version string for InspectionsLib.
+FOUNDATION_EXPORT const unsigned char InspectionsLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InspectionsLib/PublicHeader.h>
+
+
