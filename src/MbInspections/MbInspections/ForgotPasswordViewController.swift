017c251b849ae649985b375a6a2d677c0d90350a
Ryan Johansen <ryankjohansen@gmail.com>
2017-02-22 21:15:03 +0200

    Started work on Forgot password

--- src/MbInspections/MbInspections/ForgotPasswordViewController.swift
+++ src/MbInspections/MbInspections/ForgotPasswordViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  ForgotPasswordViewController.swift
 //  MbInspections
 //
 //  Created by Ryan Johansen on 2017/02/22.
@@ -8,20 +8,16 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
-    @IBOutlet weak var lblUserFullname: UILabel!
+class ForgotPasswordViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
