0f38b10771e22e3a38732ef1363762e390a8ec07
Ryan Johansen <ryankjohansen@gmail.com>
2017-02-22 14:20:26 +0200

    Setting up the final project structure and placeholders for classes

---
+++ src/InspectionsLib/InspectionsLib/FinancedAssetInspectionSheetService.swift
@@ -0,0 +1,16 @@
+//
+//  FinancedAssetInspectionSheetService.swift
+//  InspectionsLib
+//
+//  Created by Ryan Johansen on 2017/02/22.
+//  Copyright © 2017 Ryan Johansen. All rights reserved.
+//
+
+import Foundation
+
+public class FinancedAssetInspectionSheetService
+{
+    
+    
+    
+}
